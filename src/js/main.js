dbec1f7b8f3ce2e50f7f6a201376ba76e81a7890
Matt Gowie <gowie.matt@gmail.com>
2017-02-07 11:45:16 -0500

    Adds current location js, html, css

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 (function() {
 
-  function isMobile() {
+  var isMobile = function() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
   }
 
-  $(document).ready(function() {
+  var addRolloverEffect = function() {
     var $rollover = $('.the-rollover'),
         $climbingImg = $('.climbing-img'),
         $mainImg = $('.main-img');
@@ -16,5 +16,32 @@
       if (isMobile()) { return; }
       $mainImg.toggleClass('transparent');
     });
+  }
+
+  var addCurrentLocation = function() {
+    var $currentLocation = $('.current-location'),
+        $pin = $('.pin');
+    $.getJSON('https://whereami.mattgowie.com/last_location', function(data) {
+      var city = data['city'],
+          country = data['country'],
+          countryLong = data['country_long'],
+          gmapUrl = "https://www.google.com/maps/place/" + data['latitude'] + ',' + data['longitude'],
+          currentLocText;
+
+      if ((countryLong.length + city.length + 2) > 20) {
+        currentLocText = city + ", " + country;
+      } else {
+        currentLocText = city + ", " + countryLong;
+      }
+
+      $currentLocation.text(currentLocText);
+      $currentLocation.wrap("<a href='" + gmapUrl + "' title='Google Maps link to my current location' target='_blank'></a>");
+      $pin.removeClass('hidden');
+    });
+  }
+
+  $(document).ready(function() {
+    addRolloverEffect();
+    addCurrentLocation();
   });
 })();

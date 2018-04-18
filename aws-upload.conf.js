e247095cf7a209829c4f1f42a6891b90d01d4815
Matt Gowie <gowie.matt@gmail.com>
2018-04-17 21:41:30 -0400

    Adds s3-upload config + gitignore the creds file

---
+++ aws-upload.conf.js
@@ -0,0 +1,13 @@
+module.exports = {
+	credentials: "aws-credentials.json",
+	bucketName: "mattgowie.com",
+	patterns: [
+		"css/**",
+		"img/**",
+		"js/**",
+		"*.html",
+		"favicon.ico",
+		"humans.txt",
+		"robots.txt"
+	]
+}
